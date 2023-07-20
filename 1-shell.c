affeb708f863a2c9a70c8a2e6a1bb522c9cc4273
Alhadji Oumate <alhadjioum0@gmail.com>
2023-07-20 13:29:30 +0100

    shell task 1

---
+++ 1-shell.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+
+extern char **environ;
+
+void exec_word_command();
+int _puts(char *str);
+
+/**
+ * main - test code
+ * Return: nothin
+ */
+
+int main()
+{
+	while (1)
+		exec_word_command();
+}
+
+/**
+ * exec_word_command - execute a one word command
+ * Return: nothing
+ */
+
+void exec_word_command(void)
+{
+	ssize_t bufsize = 0;
+	char *line = NULL;
+	char **env = environ;
+	pid_t child_pid;
+	int status;
+
+	_puts("#cisfunc$ ");
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		_puts("Usage: simple_shell");
+		return;
+	}
+
+	char *token = strtok(line, " \n");
+	if (token == NULL)
+		return;
+		
+	char *argv[] = {token, NULL, NULL};
+
+	child_pid = fork();
+
+	if (child_pid == 0)
+	{
+		if (execve(token, argv, env) == -1)
+		{
+			perror("Error");
+		}
+	} else
+	{
+		wait(&status);
+	}
+
+	free(line);
+	line = NULL;
+}
+
+/**
+ * _puts - print a given string
+ * @str: the string to print
+ * Return: the number of characters printed
+ */
+
+int _puts(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	return (count);
+}
+
